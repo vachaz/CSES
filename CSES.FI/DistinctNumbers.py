f703fa24787e0eba6ff7a7d969c1d06eb12c766e
Vachaspati Mishra <vachaspatimishra99@gmail.com>
2026-04-10 09:38:19 +0530

    Commit

--- CSES.FI/DistinctNumbers.py
+++ CSES.FI/DistinctNumbers.py
@@ -1,13 +1,23 @@
-import sys
-class Solution:
-    def distinctnumbers(self, n, arr):
-        # return len(set(arr))
-        unique_elements = set()
-        for i in arr:
-            unique_elements.add(i)
-        return len(unique_elements)
-if __name__ == '__main__':
-    solution = Solution()
-    n = int(sys.stdin.readline())
-    arr = list(map(int, sys.stdin.readline().split()))
-    print(solution.distinctnumbers(n,arr))
+# import sys
+# class Solution:
+#     def distinctnumbers(self, n, arr):
+#         # return len(set(arr))
+#         unique_elements = set()
+#         for i in arr:
+#             unique_elements.add(i)
+#         return len(unique_elements)
+# if __name__ == '__main__':
+#     solution = Solution()
+#     n = int(sys.stdin.readline())
+#     arr = list(map(int, sys.stdin.readline().split()))
+#     print(solution.distinctnumbers(n,arr))
+n = int(input())
+# create a sorted list of the numbers
+numbers = sorted(map(int, input().split()))
+ans = 1
+for i in range(1, n):
+	# if the current number is different from the previous
+	# it is a distinct number so we add 1 to the answer
+	if numbers[i] != numbers[i - 1]:
+		ans += 1
+print(ans)
