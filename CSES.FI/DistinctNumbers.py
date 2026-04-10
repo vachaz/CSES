a9a040631efac51f3a8a7fe7ecf5269256586ab1
Vachaspati Mishra <vachaspatimishra99@gmail.com>
2026-04-10 09:37:00 +0530

    Commit

--- CSES.FI/DistinctNumbers.py
+++ CSES.FI/DistinctNumbers.py
@@ -1,7 +1,11 @@
 import sys
 class Solution:
     def distinctnumbers(self, n, arr):
-        return len(set(arr))
+        # return len(set(arr))
+        unique_elements = set()
+        for i in arr:
+            unique_elements.add(i)
+        return len(unique_elements)
 if __name__ == '__main__':
     solution = Solution()
     n = int(sys.stdin.readline())
